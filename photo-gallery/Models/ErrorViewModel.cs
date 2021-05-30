720bad9d37a3322f2aaabfff2284c23eef038146
Małgosia Sobocińska <sobocinska.malgo@gmail.com>
2021-05-30 14:20:07 +0200

    Add project files.

---
+++ photo-gallery/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace photo_gallery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
