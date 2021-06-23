03fea2bf68e9524b2ff0ad6f88c7cf2ced3d8c89
Araagoth <kertoipp@gmail.com>
2021-06-23 21:53:36 +0200

    IImageService

--- photo-gallery/Controllers/ImageController.cs
+++ photo-gallery/Controllers/ImageController.cs
@@ -4,11 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApp.Models;
+using WebApp.Services;
 
 namespace WebApp.Controllers
 {
     public class ImageController : Controller
     {
+        private readonly IImageService _imageService;
+
+
+        public async Task<IActionResult> Index()
+        {
+            var image = await _imageService.GetPhotosAsync();
+            var model = new ImagesViewModel();
+            {
+                Images = images;
+            };
+
+            return View(model);
+        }
         public IActionResult Index()
         {
             return View(new ImagesViewModel());
---
+++ photo-gallery/Services/IImageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Services
+{
+    public class IImageService
+    {
+        Task<ImageItemViewModel[]> GetPhotosAsync(string UserId);
+    }
+}
